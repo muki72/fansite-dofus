add6590485ab1165b00a5ae4b674c7caac8b4f6a
muky <muky72000@gmail.com>
2025-03-06 15:43:02 +0100

    Add Almanax feature to HomeController and update templates; replace logo and enhance registration/login forms

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,10 +19,12 @@ final class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(GuideRepository $guideRepository, PostRepository $PostRepository): Response
     {
+        $date = date("Y-m-d");
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
             'guides' => $guideRepository->findAll(),
             'postes' => $PostRepository->findAll(),
+            'almanax' => json_decode(file_get_contents("https://api.dofusdu.de/dofus3/v1/fr/almanax/$date"))
         ]);
     }
 }
