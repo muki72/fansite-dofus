9a4c56a4e022cf0ff27eade7e11d0b790e6dc854
muky <muky72000@gmail.com>
2025-03-11 11:03:28 +0100

    Refactor Almanax feature templates and controller; update base styles and enhance Almanax display

--- src/Controller/AlmanaxController.php
+++ src/Controller/AlmanaxController.php
@@ -11,23 +11,25 @@ final class AlmanaxController extends AbstractController
     #[Route('/almanax', name: 'app_almanax')]
     public function index(): Response
     {
-        $date = new \DateTime();
-        $today = $date->format('Y-m-d');
-        $month = (int)$date->format('m');
-        $year = (int)$date->format('Y');
-        $daysInMonth = (int)$date->format('t');
+        // $date = new \DateTime();
+        // $today = $date->format('Y-m-d');
+        // $month = (int)$date->format('m');
+        // $year = (int)$date->format('Y');
+        // $daysInMonth = (int)$date->format('t');
 
-        $almanaxData = [];
+        // $almanaxData = [];
 
-        for ($day = 1; $day <= $daysInMonth; $day++) {
-            $currentDate = $date->setDate($year, $month, $day)->format('Y-m-d');
-            $almanaxData[$currentDate] = json_decode(file_get_contents("https://api.dofusdu.de/dofus3/v1/fr/almanax/$currentDate"), true);
-        }
-
+        // for ($day = 1; $day <= $daysInMonth; $day++) {
+        //     $currentDate = $date->setDate($year, $month, $day)->format('Y-m-d');
+        //     $almanaxData[$currentDate] = json_decode(file_get_contents("https://api.dofusdu.de/dofus3/v1/fr/almanax/$currentDate"), true);
+        // }
+        $from = date("Y-m-d");
+        $to = date('Y-m-d', strtotime($from . ' + 29 days'));;
+        $almanaxData = json_decode(file_get_contents("https://api.dofusdu.de/dofus3/v1/fr/almanax?range[from]=$from&range[to]=$to&range[size]=-1&timezone=Europe/Paris"));
+        
         return $this->render('almanax/index.html.twig', [
-            'controller_name' => 'AlmanaxController',
             'almanaxData' => $almanaxData,
-            'today' => $today,
+
         ]);
     }
 }
